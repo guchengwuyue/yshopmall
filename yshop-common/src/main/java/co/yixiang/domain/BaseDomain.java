887be300794644b6d6796faf9fcb2bd43aba7a9b
xuwenbo <717567226@qq.com>
2020-10-09 15:24:11 +0800

    添加数据库公共模型

---
+++ yshop-common/src/main/java/co/yixiang/domain/BaseDomain.java
@@ -0,0 +1,38 @@
+package co.yixiang.domain;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Date;
+
+
+/**
+ * @ClassName 公共模型
+ * @Author hupeng <610796224@qq.com>
+ * @Date 2020/10/09
+ **/
+@Getter
+@Setter
+public class BaseDomain implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+
+    @TableField(fill= FieldFill.INSERT)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
+    private Date createTime;
+
+    @TableField(fill= FieldFill.UPDATE)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
+    private Date updateTime;
+
+    @TableLogic
+    @JsonIgnore
+    private Integer isDel;
+}
