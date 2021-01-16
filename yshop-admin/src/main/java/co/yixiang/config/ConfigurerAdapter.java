321e44b1457ebba1f7dc03b29ad9da89d68176c4
taozi <552920369@qq.com>
2021-01-16 18:01:35 +0800

    yshop2.3发布

--- yshop-admin/src/main/java/co/yixiang/config/ConfigurerAdapter.java
+++ yshop-admin/src/main/java/co/yixiang/config/ConfigurerAdapter.java
@@ -36,10 +36,22 @@ public class ConfigurerAdapter implements WebMvcConfigurer {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
+        // 允许cookies跨域
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
+        // #允许向该服务器提交请求的URI，*表示全部允许，在SpringMVC中，如果设成*，会自动转成当前请求头中的Origin
+        config.addAllowedOriginPattern("*");
+        // #允许访问的头信息,*表示全部
         config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
+        // 预检请求的缓存时间（秒），即在这个时间段里，对于相同的跨域请求不会再预检了
+        config.setMaxAge(18000L);
+        // 允许提交请求的方法类型，*表示全部允许
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("HEAD");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
